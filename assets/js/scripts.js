23e234e51939334ef90f88c4ab47a51989987bc3
lokesh2512 <lokeshgupta2512@gmail.com>
2022-03-17 00:48:02 +0530

    frt_project

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+function homeORSlider() {
+    $('#homeORSlider .slider').bxSlider({
+        pager: null,
+        speed: 3000
+    });
+}
+
+
+$( document ).ready(function() {
+    
+    homeORSlider();
+});
